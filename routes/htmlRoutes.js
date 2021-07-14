84f03d29e86bf60ef7d37ea364a3839ddbede4db
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 10:05:07 -0500

    created html routes per standards

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,14 @@
-// The following HTML routes should be created:
+const router = require('express').Router();
+const path = require('path');
 
 // * `GET /notes` should return the `notes.html` file.
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
 
 // * `GET *` should return the `index.html` file.
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
-const router = require('express').Router();
+module.exports = router;
