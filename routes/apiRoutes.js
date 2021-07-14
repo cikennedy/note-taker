6d70f6ae58ee5ce35c187d2056e62e3aa50fb11b
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 10:09:16 -0500

    structuring apiRoutes.js file with beginner code for get, post, and delete routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 
 // * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+router.get('/notes', (req, res) => {
 
+});
 
 // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and 
 // then return the new note to the client. 
 // You'll need to find a way to give each note a unique id when it's saved 
 // (look into `npm` packages that could do this for you).
+
+router.post('/notes', (req, res) => {
+
+});
 
 
 // You haven’t learned how to handle DELETE requests, but this application has that functionality in the front end. 
@@ -15,5 +21,9 @@ const router = require('express').Router();
 // * `DELETE /api/notes/:id` should receive a query parameter containing the id of a note to delete. In order to 
 // delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` 
 // property, and then rewrite the notes to the `db.json` file.
+
+router.delete('/notes/:id', (req, res) => {
+
+});
 
 module.exports = router;
