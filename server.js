c4157805da51b1185a539238fb34760d1eda3d2f
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 10:12:23 -0500

    added routes to server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+// Use the express module and require the relevant route files
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // Begin the app and state the port 
 const app = express();
@@ -8,6 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// Routes 
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // Start server, console.log the port 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
