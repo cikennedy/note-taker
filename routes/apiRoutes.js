87effd0d162e08d32c471d03913c3f30a0242f51
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 10:54:57 -0500

    tested POST functionality successfully, added delete functionality and will test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -33,16 +33,20 @@ router.post('/notes', (req, res) => {
     });
 });
 
-
-// You haven’t learned how to handle DELETE requests, but this application has that functionality in the front end. 
-// As a bonus, see if you can add the DELETE route to the application using the following guideline:
-
 // * `DELETE /api/notes/:id` should receive a query parameter containing the id of a note to delete. In order to 
 // delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` 
 // property, and then rewrite the notes to the `db.json` file.
-
 router.delete('/notes/:id', (req, res) => {
-
+    const noteId = req.params.id;
+    const noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+    const readData = noteData.filter(note => note.id.toString() !== noteId);
+    // rewrite the readData to db.json file 
+    fs.writeFileSync("./db/db.json", JSON.stringify(readData));
+    res.json(readData)
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
