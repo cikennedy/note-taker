cfff96c142d6d4049d49333e2d2f2ef5b0b505e4
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 09:56:51 -0500

    created routes directory with relevant files, comments, and initial router starter code

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,7 @@
+// The following HTML routes should be created:
+
+// * `GET /notes` should return the `notes.html` file.
+
+// * `GET *` should return the `index.html` file.
+
+const router = require('express').Router();
